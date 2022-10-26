f012d75cd47bf09384b6333fcbe3b92b95490402
ddorci <ddorci@humatics.com>
2022-10-25 22:33:26 -0400

    part 2 of ride-hailing service complete

--- notuber/README.md
+++ notuber/README.md
@@ -1,7 +1,14 @@
-## Assignment Requirements
+## Assignment Requirements Part (1)
 - All needed parts have been implemented correctly (google maps api used, cars positioned correctly, speed optimizations recorded)
 - speed before optimization: 909 kB transferred, 1.6 MB resources, Finish: 3.02 s, DOMContentLoaded: 1.06 s
 - speed after optimization:  909 kB transferred, 1.6 MB resources, Finish: 1.87 s, DOMContentLoaded: 442 ms
 - speed optimization included all things list in Canvas requirements.
 - Worked with no one.
 - 1.5 hr working on the lab.
+
+## Assignment Requirements Part (2)
+- All needed parts have been implemented correctly (found and marked position, make successful request to the ride-hailing API, marked all vehicles, infowindow for distance to closest, polyline made to closest)
+- Only one call to ride-hailing API and Google Maps API were used.
+- Implemented "Going Beyond" bullet point 2, each car has a clickable infowindow that displays its distance to the user.
+- Worked with no one. Used the provided Stackoverflow post:  https://stackoverflow.com/questions/9713058/sending-post-data-with-a-xmlhttprequest and Google Maps API docs.
+- 2.5 hrs working on the lab. 
--- notuber/map.js
+++ notuber/map.js
@@ -1,22 +1,136 @@
 let map;
+let userCoords;
+
+const iconBase =
+  "";
+const icons = {
+  car: {
+    icon: iconBase + "car.png",
+  },
+};
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+
+  userCoords = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+
+  map.setCenter(userCoords);
+
+  let yourMarker = new google.maps.Marker({
+    position: userCoords,
+    label: {text:"You", className: "marker"},
+    map: map});
+
+  var http = new XMLHttpRequest();
+  var url = 'https://jordan-marsh.herokuapp.com/rides';
+  var params = 'username=xXoDw780&lat=' + userCoords.lat() + '&lng=' + userCoords.lng();
+  http.open('POST', url, true);
+
+  //Send the proper header information along with the request
+  http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+  http.onreadystatechange = function() {//Call a function when the state changes.
+      if(http.readyState == 4 && http.status == 200) {
+
+          let minDist = Infinity;
+          let closestCar;
+          let closestCarName;
+          let jsonData = JSON.parse(http.responseText);
+
+            // Create markers.
+            for (let i = 0; i < jsonData.length; i++) {
+
+              let latLng = new google.maps.LatLng(jsonData[i].lat, jsonData[i].lng);
+
+              let dist = google.maps.geometry.spherical.computeDistanceBetween(userCoords, latLng)*0.000621371;
+
+              if(minDist >= dist){
+                minDist = dist;
+                closestCar = latLng;
+                closestCarName = jsonData[i].username;
+              }
+
+              let marker = new google.maps.Marker({
+                position: latLng,
+                icon: icons.car.icon,
+                label: {text:jsonData[i].username, className: "marker"},
+                map: map,
+              });
+
+              let contentString =
+                '<div id="content">' +
+                '<h1 id="firstHeading" class="firstHeading"> Car: ' + jsonData[i].username + '\'s Distance</h1>' +
+                '<div id="bodyContent">' +
+                "<p>The car: " + jsonData[i].username + " is approximately " + dist.toFixed(2) + " miles from your location.</p>" +
+                "</div>" +
+                "</div>";
+              let infowindow = new google.maps.InfoWindow({
+                content: contentString,
+                ariaLabel: jsonData[i].username + " Location",
+                pixelOffset: google.maps.Size(0, 0)
+              });
+
+              marker.addListener("click", () => {
+                infowindow.open({
+                  anchor: marker,
+                  map,
+                });
+              });
+
+            }
+
+          const closestPath = new google.maps.Polyline({
+            path: [userCoords, closestCar],
+            geodesic: true,
+            strokeColor: "#FF0000",
+            strokeOpacity: 1.0,
+            strokeWeight: 2,
+          });
+
+          closestPath.setMap(map);
+
+          let contentString =
+            '<div id="content">' +
+            '<h1 id="firstHeading" class="firstHeading">Your Location</h1>' +
+            '<div id="bodyContent">' +
+            "<p>The car: " + closestCarName + " is the closest vehicle to your location. It is approximately " + minDist.toFixed(2) + " miles away.</p>" +
+            "</div>" +
+            "</div>";
+          let infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            ariaLabel: "Your Location",
+            pixelOffset: google.maps.Size(0, 0)
+          });
+
+          yourMarker.addListener("click", () => {
+            infowindow.open({
+              anchor: yourMarker,
+              map,
+            });
+          });
+      }
+  }
+  http.send(params);
+
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
 
 function initMap() {
+
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 42.352271, lng: -71.05524200000001 },
     zoom: 14,
   });
 
-
-  const iconBase =
-    "";
-  const icons = {
-    car: {
-      icon: iconBase + "car.png",
-    },
-  };
-
-
-  const features = [
+/*  const features = [
     {
       position: new google.maps.LatLng(42.3453, -71.0464),
       type: "car",
@@ -58,8 +172,9 @@ function initMap() {
       anchorPoint: new google.maps.Point(515, 12),
       map: map,
     });
-  }
+  }*/
 
 }
 
 window.initMap = initMap;
+navigator.geolocation.getCurrentPosition(success, error, options);
